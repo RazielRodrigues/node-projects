438db12e50761dfe857d0640c98a6b962a81e3b0
Raziel Rodrigues <raziel.rodrigues@pjbank.com.br>
2022-07-06 12:03:30 -0300

    creating routes

---
+++ crud-by-myself/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors')
+const server = express();
+const port = process.env.PORT || 3333
+
+server.use(cors({origin: 'http://localhost:3334'}))
+server.use(express.json())
+server.use(express.urlencoded())
+
+// Routes
+const travelRoutes = require('./src/routes/travel');
+server.use('/travel', travelRoutes);
+
+server.listen(port, () => {
+    console.log('Server on: port 3333');
+})
+
+module.exports = server;
