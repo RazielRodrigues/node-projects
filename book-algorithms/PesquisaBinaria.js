2a1e1256b1f2e2f9a3cc69480b0f994c355a0a6c
Raziel Rodrigues <razielx3@live.com>
2022-09-16 20:01:04 -0300

    pesqusia binaria

---
+++ book-algorithms/PesquisaBinaria.js
@@ -0,0 +1,28 @@
+class PesquisaBinaria {
+ 
+    pesquisar(lista, item){
+
+        let baixo = 0;
+        let alto = lista.length - 1;
+
+        while(baixo <= alto){
+            let meio = (baixo + alto) / 2;
+            let chute = lista[meio];
+
+            if(chute == item){
+                console.log(`valor encontrado: ${meio}`);
+            }
+            
+            if(chute > item){
+                alto = meio - 1;
+            }else{
+                baixo = meio + 1;
+            }
+
+        }
+
+    }
+
+}
+
+(new PesquisaBinaria).pesquisar([1,2,3], 3);
